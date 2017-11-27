bd5f9b491e97960d1ae58d09e7e02683b3b03220
Malpp <cpt.ortan@gmail.com>
2017-11-27 15:40:28 -0500

    Screenshot function

--- Screenshot/Program.cs
+++ Screenshot/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -32,11 +35,30 @@ public class MyCustomApplicationContext : ApplicationContext
         trayIcon = new NotifyIcon()
         {
             Icon = Resources.AppIcon,
-            ContextMenu = new ContextMenu(new MenuItem[] {
+            ContextMenu = new ContextMenu(new MenuItem[]
+            {
+                new MenuItem("Screenshot", Screenshot),
                 new MenuItem("Exit", Exit)
             }),
             Visible = true
         };
+    }
+
+    private void Screenshot(object sender, EventArgs e)
+    {
+        //trayIcon.Visible = false;
+
+        int totalHeight = 0;
+        int totalWidth = 0;
+        Screen.AllScreens.ToList().ForEach(screen => totalHeight += screen.Bounds.Height);
+        Screen.AllScreens.ToList().ForEach(screen => totalWidth += screen.Bounds.Width);
+        Bitmap printscreen = new Bitmap(totalWidth, totalHeight);
+
+        Graphics graphics = Graphics.FromImage(printscreen as Image);
+        graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+        //save graphic variable into memory
+        printscreen.Save("C:/asd.png", ImageFormat.Png);
     }
 
     void Exit(object sender, EventArgs e)
